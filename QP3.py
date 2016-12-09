108d0f796c23489650afb7865a8f7e8d9101b7bd
RyunosukeT <thr-ryu@live.jp>
2016-12-09 16:21:13 +0900

    add class, comment

--- QP3.py
+++ QP3.py
@@ -1,58 +1,74 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+QP-3(X)のUSB通信制御用プログラムです
+"""
+# Date: ${YEAR}/${MONTH}/${DAY}
+# Filename: ${NAME}
+__author__ = 'RyunosukeT'
+__date__ = '2016/12/9'
+__version__ = '0.1'
+
 import serial
 import time
 
-ser = None
-res = None
 
+class USB:
+    """
+    USB通信を制御するクラス、自動テスト向き
+    """
 
-def serial_open(port_number):
-    global ser
-    ser = serial.Serial(port=port_number, baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=0.1, write_timeout=0.1)
+    serialport = None  # QP-3と繋がるシリアルポート、オブジェクト
+    response = None  # QP-3からのレスポンス
 
+    def serial_open(self, port_number):
+        self.serialport = serial.Serial(port=port_number, baudrate=9600, bytesize=8, parity='N',
+                                        stopbits=1, timeout=0.1, write_timeout=0.1)
 
-def serial_close():
-    ser.close()
+    def serial_close(self):
+        self.serialport.close()
 
+    def com(self, command):
+        """汎用コマンド制御"""
+        command = command.encode('utf-8')
+        self.serialport.reset_input_buffer()  # レスポンスバッファをリセット、write直後のレスポンスを読みたいから
+        self.serialport.write(command)
+        self.response = self.serialport.readline()
+        if __name__ == '__main__':  # コード作成時のテスト用
+            print(self.response)
 
-def com(command):
-    global res
-    command = command.encode('utf-8')
-    ser.reset_input_buffer()
-    ser.write(command)
-    res = ser.readline()
-#    print(res)
+    def oscillator_ABorBA_setup(self, AB_BA):
+        """オシレータモードAB相/BA相セットアップ選択、0または1"""
+        AB_BA = AB_BA.encode('utf-8')  # ABなら0、BAなら1
+        self.com('G0+050\r')
+        self.com('G1+000\r')
+        self.com('G2' + AB_BA + '\r')
+        self.com('G30\r')
 
+    def oscillator_AB_setup(self):
+        """オシレータモードAB相セットアップ、freq_testの関係で存在するムダ"""
+        self.oscillator_ABorBA_setup('0')
 
-def oscillator_AB_setup():
-    com('G0+050\r')
-    com('G1+000\r')
-    com('G20\r')
-    com('G30\r')
+    def oscillator_BA_setup(self):
+        """オシレータモードBA相セットアップ、freq_testの関係で存在するムダ"""
+        self.oscillator_ABorBA_setup('1')
 
+    def oscillator(self, frequency):
+        """オシレータモード出力"""
+        command = str('N0' + frequency + '\r')
+        self.com(command)
+        command = 'T3\r'
+        self.com(command)
+        time.sleep(1)  # ハードの動作時間があるので余裕を持たせる
 
-def oscillator_BA_setup():
-    com('G0+050\r')
-    com('G1+000\r')
-    com('G21\r')
-    com('G30\r')
+    def program_in(self):
+        """Hello program mode"""
+        command = 'P\r'
+        self.com(command)
 
-
-def oscillator(f):
-    command = str('N0' + f + '\r')
-    com(command)
-    command = 'T3\r'
-    com(command)
-    time.sleep(1)
-
-
-def program_in():
-    command = 'P\r'
-    com(command)
-
-
-def program_out():
-    command = 'E\r'
-    com(command)
-
+    def program_out(self):
+        """Goodbye program mode"""
+        command = 'E\r'
+        self.com(command)
 
 
--- TDP39.py
+++ TDP39.py
@@ -1,56 +1,83 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+TDP-39XXのUSB通信制御用プログラムです
+"""
+# Date: ${YEAR}/${MONTH}/${DAY}
+# Filename: ${NAME}
+__author__ = 'RyunosukeT'
+__date__ = '2016/12/9'
+__version__ = '0.1'
+
+
 import serial
 import time
 
-ser = None
-res = None
+
+class RS232C:
+    """
+    RS232C通信を制御するクラス、自動テスト向き
+    """
+    serialport = None  # TDP39XXとつながるシリアルポート、オブジェクト
+    response = None
+
+    def serial_open(self, port_number):
+        self.serialport = serial.Serial(port=port_number, baudrate=2400, bytesize=8,
+                                        parity='N', stopbits=1, timeout=1, write_timeout=1)
+
+    def serial_close(self):
+        self.serialport.close()
+
+    def com(self, command):
+        """汎用コマンド制御"""
+        command = command.encode('utf-8')
+        self.serialport.reset_input_buffer()
+        self.serialport.write(command)
+        self.response = self.serialport.readline()
+        if __name__ == '__main__':  # コード作成時のテスト用
+            print(self.response)
+
+    def read(self):
+        self.com('O')
+        self.response = self.response[:8]
+        """
+        変更しました、未テスト
+        command = 'O'
+        command = command.encode('utf-8')
+        self.serialport.reset_input_buffer()
+        self.serialport.write(command)
+        self.response = self.serialport.read(8)
+        if __name__ == '__main__':  # コード作成時のテスト用
+            print(self.response)
+        """
+
+    def data_stop(self):
+        """データ垂れ流しをストップ"""
+        self.com('S')
+        time.sleep(1)  # データストップのコマンドを送ってから応答時間が必要でしょ
+
+    def program_in(self):
+        """Hello program mode"""
+        self.com('P')
+
+    def program_out(self):
+        """Goodbye program mode"""
+        self.com('E\r')
+
+    def refresh_time(self, time):
+        """koushin_time"""
+        command = str('WP04,' + time + '\r')
+        self.com(command)
+
+    def pulse_mode(self, mode):
+        """モード選択は0or1or2"""
+        command = str('WP30,' + mode + '\r')
+        self.com(command)
 
 
-def serial_open(port_number):
-    global ser
-    ser = serial.Serial(port=port_number, baudrate=2400, bytesize=8, parity='N', stopbits=1, timeout=1, write_timeout=1)
+if __name__ == '__main__':  # コード作成時のテスト用
+    response = '9000000000000\r\n'
+    print(response)
+    response = response[:8]
+    print(response)
 
-
-def serial_close():
-    ser.close()
-
-
-def com(command):
-    global res
-    command = command.encode('utf-8')
-    ser.reset_input_buffer()
-    ser.write(command)
-    res = ser.readline()
-#    print(res)
-
-
-def read():
-    global res
-    command = 'O'
-    command = command.encode('utf-8')
-    ser.reset_input_buffer()
-    ser.write(command)
-    res = ser.read(8)
-#    print(res)
-
-
-def data_stop():
-    com('S')
-    time.sleep(1)
-
-
-def program_in():
-    com('P')
-
-
-def program_out():
-    com('E\r')
-
-
-def koushin_time(t):
-    command = str('WP04,' + t + '\r')
-    com(command)
-
-
-def pulse_mode(mode):
-    command = str('WP30,' + mode + '\r')
-    com(command)
