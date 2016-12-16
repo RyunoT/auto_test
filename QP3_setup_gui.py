a477a97f3a1846ebc81b364d2a74c610cdedad25
RyunosukeT <thr-ryu@live.jp>
2016-12-16 11:09:39 +0900

    version 0.2 created

--- QP3_setup_gui.py
+++ QP3_setup_gui.py
@@ -14,140 +14,62 @@ import time
 
 
 def connect_qp3x():
-    editbox = tk.Entry(width=20)
-    editbox.grid(row=0, column=0, padx=10, pady=50, sticky='')
+    entrybox = tk.Entry(width=20)
+    entrybox.grid(row=0, column=0, padx=10, pady=50, sticky='')
 
     def ser():
         """serial open"""
-        edit_input = editbox.get()
+        entry_input = entrybox.get()
         try:
-            qp3.serial_open(edit_input)
+            qp3.serial_open(entry_input)
         except:
-            tkmsg.showinfo('Error', 'ポート番号が違います')
+            tkmsg.showinfo('Error', 'Port Number Error!!')
 
     # ボタン
-    button1 = tk.Button(text='シリアルポート接続')
+    button1 = tk.Button(text='Connect QP-3X')
     button1['command'] = ser
     button1.grid(row=0, column=1, padx=0, pady=0, sticky='')
 
 
-def allmode():
-    """QP3 allmode setup"""
-    edit1 = tk.Entry(width=10)
-    edit1.grid(row=1, column=0, padx=10, pady=10, sticky='e')
-    button1 = tk.Label(text="High_V")
-    #button1['command'] = button1
-    button1.grid(row=1, column=1, padx=0, pady=0, sticky='w')
+class Entry_Label_Button(object):
+    """left Entry right Label, and bottom Button"""
+    def __init__(self, column, row_count, label_text, button_text):
+        self.qp3 = QP3.USB()
+        self.column = column
+        self.row_count = row_count
+        self.label_text = []
+        self.label_text = label_text
+        self.entry_list = [tk.Entry(width=10)]*self.row_count
+        self.button_text = button_text
 
-    edit2 = tk.Entry(width=10)
-    edit2.grid(row=2, column=0, padx=10, pady=10, sticky='e')
-    button2 = tk.Button(text="High_V")
-    button2['command'] = button2
-    button2.grid(row=2, column=1, padx=0, pady=0, sticky='w')
+    def setting_entry(self):
+        for row in range(self.row_count):
+            self.entry_list[row] = tk.Entry(width=10)
+            self.entry_list[row].grid(row=row+1, column=self.column, padx=10, pady=10, sticky='e')
 
-    edit3 = tk.Entry(width=10)
-    edit3.grid(row=3, column=0, padx=10, pady=10, sticky='e')
-    button3 = tk.Button(text="A_B")
-    button3['command'] = button3
-    button3.grid(row=3, column=1, padx=0, pady=0, sticky='w')
+    def setting_label(self):
+        label_list = [tk.Label]*self.row_count
+        for row in range(self.row_count):
+            label_list[row] = tk.Label(text=self.label_text[row])
+            label_list[row].grid(row=row+1, column=self.column+1, padx=0, pady=0, sticky='w')
 
-    edit4 = tk.Entry(width=10)
-    edit4.grid(row=4, column=0, padx=10, pady=10, sticky='e')
-    button4 = tk.Button(text="Output_mode")
-    button4['command'] = button4
-    button4.grid(row=4, column=1, padx=0, pady=0, sticky='w')
+    def setting_button(self):
+        entry_text = [float] * self.row_count
 
-    def button_all():
-        """allmode setup button"""
-        input1 = float(edit1.get())
-        input2 = float(edit2.get())
-        input3 = float(edit3.get())
-        input4 = float(edit4.get())
-        qp3.program_in()
-        qp3.setup(input1, input2,
-                          input3, input4)
+        def button_all():
+            for row in range(self.row_count):
+                entry_text[row] = float(self.entry_list[row].get())
+                qp3.program_in()
+                qp3.allmode_setup(entry_text[0], entry_text[1], entry_text[2], entry_text[3])
 
-    button5 = tk.Button(text="Output_setup")
-    button5['command'] = button_all
-    button5.grid(row=5, column=1, padx=0, pady=0, sticky='')
+        button = tk.Button(text=self.button_text)
+        button['command'] = button_all
+        button.grid(row=5, column=self.column+1, padx=0, pady=0, sticky='')
 
-
-def oscillator():
-    edit1 = tk.Entry(width=10)
-    edit1.grid(row=1, column=2, padx=10, pady=10, sticky='e')
-
-    def oscillator_button():
-        """oscillator mode button"""
-        oscillator_input1 = float(edit1.get())
-        qp3.program_in()
-        qp3.oscillator(oscillator_input1)
-
-    button1 = tk.Button(text="Oscillator_Frequency_Hz")
-    button1['command'] = oscillator_button
-    button1.grid(row=1, column=3, padx=0, pady=0, sticky='w')
-
-def sweep():
-    edit1 = tk.Entry(width=10)
-    edit1.grid(row=1, column=4, padx=10, pady=10, sticky='e')
-    button1 = tk.Button(text="Start_Hz")
-    button1['command'] = button1
-    button1.grid(row=1, column=5, padx=0, pady=0, sticky='w')
-
-    edit2 = tk.Entry(width=10)
-    edit2.grid(row=2, column=4, padx=10, pady=10, sticky='e')
-    button2 = tk.Button(text="Stop_Hz")
-    button2['command'] = button2
-    button2.grid(row=2, column=5, padx=0, pady=0, sticky='w')
-
-    edit3 = tk.Entry(width=10)
-    edit3.grid(row=3, column=4, padx=10, pady=10, sticky='e')
-    button3 = tk.Button(text="Sweep_time_sec")
-    button3['command'] = button3
-    button3.grid(row=3, column=5, padx=0, pady=0, sticky='w')
-
-    edit4 = tk.Entry(width=10)
-    edit4.grid(row=4, column=4, padx=10, pady=10, sticky='e')
-    button4 = tk.Button(text="Wave_mode")
-    button4['command'] = button4
-    button4.grid(row=4, column=5, padx=0, pady=0, sticky='w')
-
-    def sweep_button_all():
-        """sweep setup button"""
-        sweep_input1 = float(edit1.get())
-        sweep_input2 = float(edit2.get())
-        sweep_input3 = float(edit3.get())
-        sweep_input4 = float(edit4.get())
-        qp3.program_in()
-        qp3.sweep_setup(sweep_input1, sweep_input2,
-                        sweep_input3, sweep_input4)
-
-    button5 = tk.Button(text="Sweep_setup")
-    button5['command'] = sweep_button_all
-    button5.grid(row=5, column=5, padx=0, pady=0, sticky='')
-
-def Npulse():
-    edit1 = tk.Entry(width=10)
-    edit1.grid(row=1, column=6, padx=10, pady=10, sticky='e')
-    button1 = tk.Button(text="Frequency_Hz")
-    button1['command'] = button1
-    button1.grid(row=1, column=7, padx=0, pady=0, sticky='w')
-
-    edit2 = tk.Entry(width=10)
-    edit2.grid(row=2, column=6, padx=10, pady=10, sticky='e')
-    button2 = tk.Button(text="count")
-    button2['command'] = button2
-    button2.grid(row=2, column=7, padx=0, pady=0, sticky='w')
-
-    def Npulse_button_all():
-        """sweep setup button"""
-        Npulse_input1 = float(edit1.get())
-        Npulse_input2 = float(edit2.get())
-        qp3.program_in()
-        qp3.Npulse_setup(Npulse_input1, Npulse_input2)
-
-    button3 = tk.Button(text="Npulse_setup")
-    button3['command'] = Npulse_button_all
-    button3.grid(row=5, column=7, padx=0, pady=0, sticky='')
+    def setting_ELB(self):
+        self.setting_entry()
+        self.setting_label()
+        self.setting_button()
 
 
 def quit():
@@ -160,23 +82,46 @@ def quit():
             pass
         root.destroy()
 
-    button2 = tk.Button(text="QUIT", command=quit_command)
+    button2 = tk.Button(text="Disconnect and Quit app", command=quit_command)
     button2.grid(row=6, column=1, padx=0, pady=50, sticky='')
 
 
 root = tk.Tk()
-
-
 root.title("QP-3 setup Application")
 root.geometry("1600x600")
 root.grid()
-
 qp3 = QP3.USB()
+
+# serial connect GUI
 connect_qp3x()
-allmode()
-oscillator()
-sweep()
-Npulse()
+
+# output setting GUI
+output_label = ['High(V)\r[-12.0 ~ +12.0]', 'Low(V)\r[-12.0 ~ +12.0]',
+                'AB mode\r[0:A-B, 1:B-A]', 'Output\r[0=Var.V, 1=Semi Open]']
+output_setup = Entry_Label_Button(column=0, row_count=4, label_text=output_label,
+                                  button_text='Output setting')
+output_setup.setting_ELB()
+
+# oscillator mode GUI
+oscillator_label = ['Oscillator(Hz)\r[0.001 ~ 600000]']
+oscillator_setup = Entry_Label_Button(column=2, row_count=1, label_text=oscillator_label,
+                                      button_text='Oscillator mode')
+oscillator_setup.setting_ELB()
+
+# sweep mode GUI
+sweep_label = ['Start(Hz)\r[0.1 ~ 200000]', 'Stop(Hz)\r[0.1 ~ 200000]',
+               'Sweep time(sec)\r[0.01 ~ 999.99]', 'Wave mode\r[0=Triangle,\r1=Saw, 2=Square]']
+sweep_setup = Entry_Label_Button(column=4, row_count=4, label_text=sweep_label,
+                                 button_text='Sweep mode')
+sweep_setup.setting_ELB()
+
+# Npulse mode GUI
+npulse_label = ['Frequency(Hz)\r[0.1 ~ 3000]', 'Pulse count\r[1 ~ 60000]']
+npulse_setup = Entry_Label_Button(column=6, row_count=2, label_text=npulse_label,
+                                  button_text='Npulse mode')
+npulse_setup.setting_ELB()
+
+# quit button GUI
 quit()
 
 root.mainloop()
